fbed40349825db52bdd1abce66a8e91aaf66275f
eatonwm22 <57016867+eatonwm22@users.noreply.github.com>
2019-12-16 18:21:30 -0600

    Update States.java

--- States.java
+++ States.java
@@ -1,6 +1,8 @@
+//UKNOWN PERSON
+//WILL (all I did was added the one state that was missing)
 
 public enum States {
-	AL(1), AK(2), AZ(3), AR(4), CA(5), CO(6), CT(7), DE(8), DC(9), FL(10), GA(11), HI(12), ID(13), IN(14), IA(15), KS(16), KY(17), LA(18), ME(19), MD(20), MA(21), MI(22), MN(23), MS(24), MO(25), MT(26), NE(27), NV(28), NH(29),NJ(30), NM(31), NY(32), NC(33), ND(34), OH(35), OK(36), OR(37), PA(38),RI(39), SC(40),SD(41),TN(42),TX(43),UT(44),VT(45),VA(46),WV(47),WI(48),WY(49);
+	AL(1), AK(2), AZ(3), AR(4), CA(5), CO(6), CT(7), DE(8), DC(9), FL(10), GA(11), HI(12), ID(13), IL(14) IN(15), IA(16), KS(17), KY(18), LA(19), ME(20), MD(21), MA(22), MI(23, MN(24), MS(25), MO(26), MT(27), NE(28), NV(29), NH(30),NJ(31), NM(32), NY(33), NC(34), ND(35), OH(36), OK(37), OR(38), PA(39), RI(40), SC(41),SD(42),TN(43),TX(44),UT(45),VT(46),VA(47),WV(48),WI(49),WY(50);
 	
 	private int value;
 	
